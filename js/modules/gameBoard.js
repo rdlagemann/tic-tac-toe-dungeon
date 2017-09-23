2955c90d3f97f81e5f8adc211b0e9449a2c1bd16
Rafael Lagemann <rdlagemann@gmail.com>
2017-09-22 21:41:32 -0300

    minimax implemented

--- js/modules/gameBoard.js
+++ js/modules/gameBoard.js
@@ -21,7 +21,7 @@ const gameBoard = (function() {
 		humanScore = context.document.getElementById('humanScore');
 		cpuScore = context.document.getElementById('cpuScore');
 
-		humanPlayer = player;
+		currentPlayer = humanPlayer = player;
 		cpuPlayer = playerCPU;
 
 		// extend and config table cells 
@@ -32,52 +32,37 @@ const gameBoard = (function() {
 		});
 	};
 	
-	const getFreeCells = function freecells(){
+	const getFreeCells = function freecells(cells){
 		return Array.from(cells).filter( element => element.getValue() === 'noWeapon' );
 	};
 
-	const checkWin = function check(cells) {
+	const checkWin = function check(cels) {
 		let checkTest,
 			result = {
-				win: undefined,
+				name: 'noname',
+				win: false,
 				shouldReset: false
 			}
 
 		for (let key in checker) {
 			if ( typeof checker[key] === 'function') {
-				checkTest = checker[key](cells);
+				checkTest = checker[key](cels);
 			}
 			if (checkTest.win) {
 				break;
 			}
 		}
 
-		if(checkTest.win) {
-			console.log(currentPlayer._name + ' WIN at ' + checkTest.where._index + " " + checkTest.where._orientation);
-			let winner;
-			if( currentPlayer._name === 'human') {
-				winner = humanScore;
-			}
-			else {
-				winner = cpuScore;
-			}
-
-			winner.classList.add('zoomer');
-
-			setTimeout(function() {
-				winner.classList.remove('zoomer');
-
-			}, 200);
-
-
+		if (checkTest.win) {
+			result.name = currentPlayer._name;
 			result.win = true;
 			result.shouldReset = true;
 
 			return result;
 		}
 
-		else if (!checkTest.win && (getFreeCells().length === 0)) {
-			context.alert('TIE!');
+		else if (!checkTest.win && (getFreeCells(cels).length === 0)) {
+			// console.log('checkWin: TIE');
 			
 			result.win = false;
 			result.shouldReset = true;
@@ -98,47 +83,153 @@ const gameBoard = (function() {
 	const pubReset = function reset() {
 		cpuScore.innerText = ' 0';
 		humanScore.innerText = ' 0';
+		currentPlayer = humanPlayer;
 		clearBoard();
+	};
+
+	const emptyBlocks = function(newCells) {
+		let emps = [];
+		newCells.forEach( (el, index) => {
+			// console.log('el = ', el);
+			if (el.getValue() === 'noWeapon') {
+				emps.push(index);
+			}
+		});
+
+		return emps;
 	}
 
+	const bestSpot = function bestSpot() {
+		let cellscopy = Array.from(cells).slice();
+		return minimax(cellscopy, cpuPlayer).index;
+	};
+
+	let deep = 0;
+
+	
+	const minimax = function minimax(newCells, player) {
+		let freeCells = emptyBlocks(newCells);
+				
+		result = checkWin(newCells);
+
+		deep += 1;
+
+
+
+		// stop conditions
+		if (result.name === 'human') {
+			return {score: 10};			
+		}else if (result.name === 'cpu'){			
+			return {score: -10};
+		}
+		else if(result.shouldReset){
+			return {score: 0};
+		}
+		
+
+		let moves = [];
+
+		for (let i = 0; i < freeCells.length; i += 1) {
+			let move = {};
+			
+			move.index = freeCells[i];
+			newCells[move.index].setValue(player._weapon.getValue());	
+
+					
+			if (player._name === 'cpu') {
+				currentPlayer = humanPlayer;
+				let result = minimax(newCells, humanPlayer);
+				move.score = result.score;
+			} 
+			else if(player._name === 'human') {
+				currentPlayer = cpuPlayer;
+				let result = minimax(newCells, cpuPlayer);
+				move.score = result.score;
+			}
+
+			newCells[move.index].setValue(undefined);
+
+			moves.push(move);
+		}
+
+		//move with highest score when cpu is playing
+		let bestMove;
+		
+		if (player._name === 'cpu') {
+			let bestScore = -10000;
+			for (let i = 0; i < moves.length; i += 1) {
+				if (moves[i].score > bestScore) {
+					bestScore = moves[i].score;
+					bestMove = i;
+				}
+			}
+		}
+		//move with lowest score when human is playing 
+		else if(player._name === 'human'){
+			let bestScore = 10000;
+			for (let i = 0; i < moves.length; i += 1) {
+				if (moves[i].score < bestScore) {
+					bestScore = moves[i].score;
+					bestMove = i;
+				}
+			}
+		}
+
+		// console.log(moves[bestMove]);
+
+		return moves[bestMove];	
+
+	};
 	
 	const cpuPlay = function playcpu() {
 		// TODO: Implement AI
-		let freeCells = getFreeCells();
+		let freeCells = getFreeCells(cells);
 
 		if (freeCells.length <= 1) {
 			return;
 		}
 
-		currentPlayer = cpuPlayer;				
+		let result, move;
 
-		let len = cells.length,
-			index = 0,
-			isValid = false,
-			result;
+		currentPlayer = cpuPlayer;	
 
-		while (!isValid) {
-		
-			index = Math.floor( Math.random() * freeCells.length );
+		console.log(freeCells.length);
 
-			if (freeCells[index].getValue() === 'noWeapon') {
-				isValid = true;
-				freeCells[index].setValue( cpuPlayer._weapon.getValue() );
-				freeCells[index].draw();
-
-				result = checkWin( cells );
-
-				if (result.win) {
-					cpuScore.innerText = ' ' + (parseInt(cpuScore.innerText) + 1);
-					clearBoard();
-				}
-				else if (result.shouldReset) {
-					clearBoard();
+		if (freeCells.length === 8) {
+			for (let i = 0; i < cells.length; i += 2) {
+				if (cells[i].getValue() !== 'noWeapon') {
+					if (i % 2 === 0 && i !== 4) {
+						move = 4;
+						break;
+					}
+					else if (i === 4) {
+						move = 0;
+						break;
+					}
 				}
 			}
 		}
+		else {
+			move = bestSpot();
+		}
+		
+		cells[move].setValue( cpuPlayer._weapon.getValue() );
+		cells[move].draw();		
+
+		result = checkWin(cells);	
+
+		console.log(result);
+
+		if (result.win) {
+			cpuScore.innerText = ' ' + (parseInt(cpuScore.innerText) + 1);
+			clearBoard();
+		}
+		else if (result.shouldReset) {
+			clearBoard();
+		}
 
 		currentPlayer = humanPlayer;
+		
 	};
 
 	const playWeapon = function playWeapon() {
@@ -146,6 +237,7 @@ const gameBoard = (function() {
 			
 			this.setValue( humanPlayer._weapon.getValue() );
 			this.draw();
+
 
 			let result = checkWin( cells );
 
